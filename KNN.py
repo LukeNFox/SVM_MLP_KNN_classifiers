16ba730d478fad6154e88e9073d1585ea1dd7ecb
Luke Fox <l.fox9@nuigalway.ie>
2019-10-03 19:04:12 +0100

    implementing KNN for dataset

--- KNN.py
+++ KNN.py
@@ -15,8 +15,17 @@ variety = le.fit_transform(list(data[predict]))
 x = np.array(data.drop([predict], 1))
 y = list(variety)
 
-
-
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 
-print(x_train, y_test)
+model = KNeighborsClassifier(n_neighbors=7)
+
+model.fit(x_train, y_train)
+
+acc = model.score(x_test, y_test)
+print(acc)
+
+predicted = model.predict(x_test)
+names = ["c_avellana","c_americana","c_cornuta"]
+
+for x in range(len(predicted)):
+    print("Predicted: ", names[predicted[x]],  "Actual: ", names[y_test[x]])
